6c1c0a9d79a854014021b93849932ac119c4e136
Noemi Walzebuck <noemi@mapbox.com>
2016-07-10 18:31:41 -0700

    last class updates

---
+++ functions/JS/functions.js
@@ -0,0 +1,39 @@
+console.log("template loaded");
+
+console.log("functions")
+
+function subtract(x,y) {
+
+	var difference = x - y;
+	return difference;
+
+}
+
+// you could use x,y again as the input in your function
+
+function multiply(a,b) { 
+
+	var product = a * b;
+	return product;
+
+}
+
+
+function add(c,d) {
+
+	var addition = c + d;
+	return addition
+
+}
+
+function divide(e,f) {
+
+	var division = e / f;
+	return division
+
+}
+
+function remain(g,h) {
+	 var remainder = g % h;
+	 return	 remainder
+}
