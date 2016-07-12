e626975d3c74c1c4189123db15b931d884a5425c
Noemi Walzebuck <noemi@mapbox.com>
2016-07-11 18:53:43 -0700

    styling

--- calculators/js/formulas.js
+++ calculators/js/formulas.js
@@ -6,21 +6,22 @@
 // 3.	 Be sure to link up this file in your HTML doc
 /////////////////////////////////////////////////////////////////////
 
+// Pythagorean theorem - defining `calcLongestSide`
 function calcLongestSide(valueA, valueB){
 	return Math.sqrt ((valueA*valueA) + (valueB*valueB));
 }
 
-
+// Radius to circumference - defining `calcCircumference`
 function calcCircumference(rvalue){
 	return (2 * Math.PI * rvalue);
 }
 
-
+// Fahrenheit to celsius - defining `calcFahrenheitToCelsius`
 function calcFahrenheitToCelcius(tempFvalue){
 	return ((tempFvalue - 32) * (5/9));
 }
 
-
+// Celsius to Fahrenheit - defining `calcCelsiusToFahrenheit`
 function calcCelciusToFarenheit(tempCvalue){
-	return ((tempCvalue * (9/5)) + 32)
+	return ((tempCvalue * (9/5)) + 32);
 }
